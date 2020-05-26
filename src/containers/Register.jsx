9fc77156c74b09dd3b21901ab8cec19ef421d148
LucasAlda <lucas@aldazabal.com.ar>
2020-05-25 21:44:22 -0300

    funcionalidad register. creacion player.

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
-import '../assets/styles/components/Register.scss';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { registerRequest } from '../actions';
+import '../assets/styles/components/Register.scss';
 
-const Login = () => (
-  <section className="register">
-    <section className="register__container">
-      <h2>Regístrate</h2>
-      <form className="register__container--form">
-        <input className="input" type="text" placeholder="Nombre" />
-        <input className="input" type="text" placeholder="Correo" />
-        <input className="input" type="password" placeholder="Contraseña" />
-        <button className="button">Registrarme</button>
-      </form>
-      <Link to="/login">Iniciar sesión</Link>
+const Register = (props) => {
+  const [ form, setValues ] = useState({
+    email: '',
+    name: '',
+  });
+
+  const handleInput = e => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
+  }
+
+  return(
+    <section className="register">
+      <section className="register__container">
+        <h2>Regístrate</h2>
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input 
+            name="name"
+            className="input" 
+            type="text" 
+            placeholder="Nombre"
+            onChange={handleInput} 
+          />
+          <input 
+            name="email"
+            className="input" 
+            type="text" 
+            placeholder="Correo" 
+            onChange={handleInput}
+          />
+          <input 
+            name="password"
+            className="input" 
+            type="password" 
+            placeholder="Contraseña" 
+            onChange={handleInput}
+          />
+          <button className="button">Registrarme</button>
+        </form>
+        <Link to="/login">Iniciar sesión</Link>
+      </section>
     </section>
-  </section>
-);
+  );
+}
 
-export default Login;
+const mapDispatchToProps = {
+  registerRequest
+}
+
+export default connect(null, mapDispatchToProps)(Register);
